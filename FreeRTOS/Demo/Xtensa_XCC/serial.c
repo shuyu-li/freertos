32b1127770a2489272002070f902959b8b269331
Mazen Neifer <mazen@venus.nestwave.com>
2017-07-19 16:46:23 +0200

    Port of FreeRTOS V8.2.0 to Xtensa processor cores family.

---
+++ FreeRTOS/Demo/Xtensa_XCC/serial.c
@@ -0,0 +1,258 @@
+/*
+    FreeRTOS V8.2.0 - Copyright (C) 2015 Real Time Engineers Ltd.
+    All rights reserved
+
+    VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation >>!AND MODIFIED BY!<< the FreeRTOS exception.
+
+	***************************************************************************
+    >>!   NOTE: The modification to the GPL is included to allow you to     !<<
+    >>!   distribute a combined work that includes FreeRTOS without being   !<<
+    >>!   obliged to provide the source code for proprietary components     !<<
+    >>!   outside of the FreeRTOS kernel.                                   !<<
+	***************************************************************************
+
+    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  Full license text is available on the following
+    link: http://www.freertos.org/a00114.html
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS provides completely free yet professionally developed,    *
+     *    robust, strictly quality controlled, supported, and cross          *
+     *    platform software that is more than just the market leader, it     *
+     *    is the industry's de facto standard.                               *
+     *                                                                       *
+     *    Help yourself get started quickly while simultaneously helping     *
+     *    to support the FreeRTOS project by purchasing a FreeRTOS           *
+     *    tutorial book, reference manual, or both:                          *
+     *    http://www.FreeRTOS.org/Documentation                              *
+     *                                                                       *
+    ***************************************************************************
+
+    http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
+	the FAQ page "My application does not run, what could be wrong?".  Have you
+	defined configASSERT()?
+
+	http://www.FreeRTOS.org/support - In return for receiving this top quality
+	embedded software for free we request you assist our global community by
+	participating in the support forum.
+
+	http://www.FreeRTOS.org/training - Investing in training allows your team to
+	be as productive as possible as early as possible.  Now you can receive
+	FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
+	Ltd, and the world's leading authority on the world's leading RTOS.
+
+    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
+    including FreeRTOS+Trace - an indispensable productivity tool, a DOS
+    compatible FAT file system, and our tiny thread aware UDP/IP stack.
+
+    http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
+    Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
+
+    http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
+    Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
+    licenses offer ticketed support, indemnification and commercial middleware.
+
+    http://www.SafeRTOS.com - High Integrity Systems also provide a safety
+    engineered and independently SIL3 certified version for use in safety and
+    mission critical applications that require provable dependability.
+
+    1 tab == 4 spaces!
+*/
+
+/* NOTE:  This is just a test file and not intended to be a generic 
+COM driver. */
+
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "serial.h"
+/*---------------------------------------------------------------------------*/
+
+#define serINVALID_QUEUE				( ( QueueHandle_t ) 0 )
+#define serNO_BLOCK						( ( TickType_t ) 0 )
+/*---------------------------------------------------------------------------*/
+
+static QueueHandle_t xRxedChars; 
+static QueueHandle_t xCharsForTx; 
+
+uint32_t uartControl;
+/*---------------------------------------------------------------------------*/
+
+static void vUARTInterruptHandler( void* context, uint32_t id );
+static void vUARTReceiveHandler( uint32_t status );
+static void vUARTTransmitHandler( uint32_t status );
+/*---------------------------------------------------------------------------*/
+
+xComPortHandle xSerialPortInitMinimal( unsigned long ulWantedBaud, unsigned portBASE_TYPE uxQueueLength )
+{
+	/* Create the queues used to hold Rx and Tx characters. */
+	xRxedChars = xQueueCreate( uxQueueLength, ( unsigned portBASE_TYPE ) sizeof( signed char ) );
+	xCharsForTx = xQueueCreate( uxQueueLength + 1, ( unsigned portBASE_TYPE ) sizeof( signed char ) );
+
+	/* If the queues were created correctly then setup the serial port hardware. */
+	if( ( xRxedChars != serINVALID_QUEUE ) && ( xCharsForTx != serINVALID_QUEUE ) )
+	{
+		portENTER_CRITICAL();
+		{
+			//TODO uartControl = ALTERA_AVALON_UART_CONTROL_RTS_MSK | ALTERA_AVALON_UART_CONTROL_RRDY_MSK | ALTERA_AVALON_UART_CONTROL_DCTS_MSK;
+			//TODO IOWR_ALTERA_AVALON_UART_CONTROL( UART_BASE, uartControl );
+		  
+		    /* register the interrupt handler */
+			//TODO alt_irq_register ( UART_IRQ, NULL, vUARTInterruptHandler );
+		}
+		portEXIT_CRITICAL();
+	}
+	else
+	{
+		return ( xComPortHandle ) 0;
+	}
+    return ( xComPortHandle ) 1;
+}
+/*---------------------------------------------------------------------------*/
+
+void vSerialClose( xComPortHandle xPort )
+{
+    /* Never used. */
+}
+/*---------------------------------------------------------------------------*/
+
+signed portBASE_TYPE xSerialGetChar( xComPortHandle pxPort, signed char *pcRxedChar, TickType_t xBlockTime )
+{
+	/* The port handle is not required as this driver only supports one port. */
+	( void ) pxPort;
+
+
+	/* Get the next character from the buffer.  Return false if no characters
+	are available, or arrive before xBlockTime expires. */
+	if( xQueueReceive( xRxedChars, pcRxedChar, xBlockTime ) )
+	{
+		return pdTRUE;
+	}
+	else
+	{
+		//TODO uartControl |= ALTERA_AVALON_UART_CONTROL_RRDY_MSK;
+		//TODO IOWR_ALTERA_AVALON_UART_CONTROL( UART_BASE, uartControl );
+		return pdFALSE;
+	}
+}
+/*---------------------------------------------------------------------------*/
+
+signed portBASE_TYPE xSerialPutChar( xComPortHandle pxPort, signed char cOutChar, TickType_t xBlockTime )
+{
+signed portBASE_TYPE lReturn = pdPASS;
+
+	/* Place the character in the queue of characters to be transmitted. */
+	if( xQueueSend( xCharsForTx, &cOutChar, xBlockTime ) == pdPASS )
+	{
+        /*Triggers an interrupt on every character or (down) when queue is full. */
+		//TODO uartControl |= ALTERA_AVALON_UART_CONTROL_TRDY_MSK;
+        //TODO IOWR_ALTERA_AVALON_UART_CONTROL( UART_BASE, uartControl );
+        lReturn = pdPASS;
+    }
+    else
+    {	
+		lReturn = pdFAIL;
+	}
+	return lReturn;
+}
+/*---------------------------------------------------------------------------*/
+
+void vSerialPutString( xComPortHandle pxPort, const signed char * const pcString, unsigned short usStringLength )
+{
+signed char *pxNext;
+
+	/* A couple of parameters that this port does not use. */
+	( void ) usStringLength;
+	( void ) pxPort;
+
+	/* NOTE: This implementation does not handle the queue being full as no block time is used! */
+
+	/* The port handle is not required as this driver only supports UART0. */
+	( void ) pxPort;
+
+	/* Send each character in the string, one at a time. */
+	pxNext = ( signed char * ) pcString;
+	while( *pxNext )
+	{
+		xSerialPutChar( pxPort, *pxNext, serNO_BLOCK );
+		pxNext++;
+	}
+}
+/*-----------------------------------------------------------*/
+
+static void vUARTInterruptHandler( void* context, uint32_t id )
+{
+	uint32_t status;
+
+	/* Read the status register in order to determine the cause of the 
+    interrupt. */
+	//TODO status = IORD_ALTERA_AVALON_UART_STATUS( UART_BASE );
+	
+	/* Clear any error flags set at the device */
+	//TODO IOWR_ALTERA_AVALON_UART_STATUS( UART_BASE, 0 );
+	
+	/* process a read irq */
+	//TODO if ( status & ALTERA_AVALON_UART_STATUS_RRDY_MSK )
+	{
+		vUARTReceiveHandler( status );
+	}
+	
+	/* process a write irq */
+	//TODO if ( status & ( ALTERA_AVALON_UART_STATUS_TRDY_MSK  ) )
+	{
+		vUARTTransmitHandler( status );
+	}
+}
+/*---------------------------------------------------------------------------*/
+
+static void vUARTReceiveHandler( uint32_t status )
+{
+signed char cChar;
+portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
+
+	/* If there was an error, discard the data */
+//TODO if ( status & ( ALTERA_AVALON_UART_STATUS_PE_MSK | ALTERA_AVALON_UART_STATUS_FE_MSK ) )
+	{
+		//TODO asm("break");
+		return;
+	}
+
+	/* Transfer data from the device to the circular buffer */
+	//TODO cChar = IORD_ALTERA_AVALON_UART_RXDATA( UART_BASE );
+	if ( pdTRUE != xQueueSendFromISR( xRxedChars, &cChar, &xHigherPriorityTaskWoken ) )
+	{
+		/* If the circular buffer was full, disable interrupts. Interrupts will 
+        be re-enabled when data is removed from the buffer. */
+		//TODO uartControl &= ~ALTERA_AVALON_UART_CONTROL_RRDY_MSK;
+		//TODO IOWR_ALTERA_AVALON_UART_CONTROL( UART_BASE, uartControl );
+	}
+    
+	portEND_SWITCHING_ISR( xHigherPriorityTaskWoken );
+}
+/*---------------------------------------------------------------------------*/
+
+static void vUARTTransmitHandler( uint32_t status )
+{
+signed char cChar;
+portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
+	/* Transfer data if there is some ready to be transferred */
+	if( xQueueReceiveFromISR( xCharsForTx, &cChar, &xHigherPriorityTaskWoken ) == pdTRUE )
+	{
+		//TODO IOWR_ALTERA_AVALON_UART_TXDATA( UART_BASE, cChar );
+    }
+    else
+    {
+    	//TODO uartControl &= ~ALTERA_AVALON_UART_CONTROL_TRDY_MSK;
+    }
+	
+	//TODO IOWR_ALTERA_AVALON_UART_CONTROL( UART_BASE, uartControl );
+    portEND_SWITCHING_ISR( xHigherPriorityTaskWoken );    
+}    
+/*---------------------------------------------------------------------------*/
---
+++ FreeRTOS/Demo/Xtensa_XCC/xt_clib.c
@@ -0,0 +1,326 @@
+/*******************************************************************************
+// Copyright (c) 2003-2015 Cadence Design Systems, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+--------------------------------------------------------------------------------
+*/
+
+/*
+*********************************************************************************************************
+*
+*                                          C LIBRARY TEST
+*
+* This test exercises the C library thread safety mechanism in the Xtensa port of FreeRTOSI.
+* It verifies that every task that specifies C library context save gets its own private
+* save area, and tasks that do not specify this get to share a global context area.
+*
+* This test is only meaningful if XT_USE_THREAD_SAFE_CLIB is enabled.
+* Currently this test works only for the newlib C library as it is the only one supporting
+* thread safety.
+*
+* Target  : All Xtensa configurable and Diamond preconfigured processors.
+*********************************************************************************************************
+*/
+
+#include "FreeRTOS.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#if XT_USE_THREAD_SAFE_CLIB > 0u
+#include <reent.h>
+#else
+#warning XT_USE_THREAD_SAFE_CLIB not defined, this test will do nothing.
+#endif
+
+#ifdef XT_BOARD
+#include <xtensa/xtbsp.h>
+#endif
+
+#include <testcommon.h>
+
+#include "task.h"
+#include "queue.h"
+
+/*
+*********************************************************************************************************
+*                                         SHARED DATA AND MACROS
+*********************************************************************************************************
+*/
+
+/* Stack size for tasks that do not use the C library */
+#define     TASK_STK_SIZE_MIN       (XT_STACK_MIN_SIZE + XT_STACK_EXTRA)
+
+/* Stack size for tasks that use the C library and the coprocessors */
+#define     TASK_STK_SIZE_STD       (0x1000 + XT_STACK_EXTRA_CLIB)
+
+/* Uniform prefix for reporting PASS/FAIL test results. */
+#define TEST_PFX    "Xtensa C library context switch test (xt_clib)"
+
+#define NTASKS      4
+
+unsigned int result[NTASKS];
+
+// Hack to access TCB's content (xNewLib_reent) for test purposes
+typedef struct
+{
+	volatile StackType_t	*pxTopOfStack;	/*< Points to the location of the last item placed on the tasks stack.  THIS MUST BE THE FIRST MEMBER OF THE TCB STRUCT. */
+
+	#if ( portUSING_MPU_WRAPPERS == 1 )
+		xMPU_SETTINGS	xMPUSettings;		/*< The MPU settings are defined as part of the port layer.  THIS MUST BE THE SECOND MEMBER OF THE TCB STRUCT. */
+		BaseType_t		xUsingStaticallyAllocatedStack; /* Set to pdTRUE if the stack is a statically allocated array, and pdFALSE if the stack is dynamically allocated. */
+	#endif
+
+	ListItem_t			xGenericListItem;	/*< The list that the state list item of a task is reference from denotes the state of that task (Ready, Blocked, Suspended ). */
+	ListItem_t			xEventListItem;		/*< Used to reference a task from an event list. */
+	UBaseType_t			uxPriority;			/*< The priority of the task.  0 is the lowest priority. */
+	StackType_t			*pxStack;			/*< Points to the start of the stack. */
+	char				pcTaskName[ configMAX_TASK_NAME_LEN ];/*< Descriptive name given to the task when created.  Facilitates debugging only. */ /*lint !e971 Unqualified char types are allowed for strings and single characters only. */
+
+	#if ( portSTACK_GROWTH > 0 )
+		StackType_t		*pxEndOfStack;		/*< Points to the end of the stack on architectures where the stack grows up from low memory. */
+	#endif
+
+	#if ( portCRITICAL_NESTING_IN_TCB == 1 )
+		UBaseType_t 	uxCriticalNesting; 	/*< Holds the critical section nesting depth for ports that do not maintain their own count in the port layer. */
+	#endif
+
+	#if ( configUSE_TRACE_FACILITY == 1 )
+		UBaseType_t		uxTCBNumber;		/*< Stores a number that increments each time a TCB is created.  It allows debuggers to determine when a task has been deleted and then recreated. */
+		UBaseType_t  	uxTaskNumber;		/*< Stores a number specifically for use by third party trace code. */
+	#endif
+
+	#if ( configUSE_MUTEXES == 1 )
+		UBaseType_t 	uxBasePriority;		/*< The priority last assigned to the task - used by the priority inheritance mechanism. */
+		UBaseType_t 	uxMutexesHeld;
+	#endif
+
+	#if ( configUSE_APPLICATION_TASK_TAG == 1 )
+		TaskHookFunction_t pxTaskTag;
+	#endif
+
+	#if ( configGENERATE_RUN_TIME_STATS == 1 )
+		uint32_t		ulRunTimeCounter;	/*< Stores the amount of time the task has spent in the Running state. */
+	#endif
+
+	#if ( configUSE_NEWLIB_REENTRANT == 1 )
+		/* Allocate a Newlib reent structure that is specific to this task.
+		Note Newlib support has been included by popular demand, but is not
+		used by the FreeRTOS maintainers themselves.  FreeRTOS is not
+		responsible for resulting newlib operation.  User must be familiar with
+		newlib and must provide system-wide implementations of the necessary
+		stubs. Be warned that (at the time of writing) the current newlib design
+		implements a system-wide malloc() that must be provided with locks. */
+		struct 	_reent xNewLib_reent;
+	#endif
+
+		// Truncated after this point
+
+} clibTCB_t;
+extern volatile clibTCB_t * volatile pxCurrentTCB;
+
+/*
+*********************************************************************************************************
+*                                        APPLICATION TASKS
+*********************************************************************************************************
+*/
+
+TaskHandle_t Task_TCB[NTASKS];
+
+void Task_Func(void *pdata)
+{
+    int val = (int) pdata;
+    unsigned cnt = 0;
+    void *test_p;
+    int err;
+
+    srand(val);
+
+#if XT_USE_THREAD_SAFE_CLIB > 0u
+
+    while (cnt < 400) {
+#if XSHAL_CLIB == XTHAL_CLIB_XCLIB
+        // Nothing yet
+#elif XSHAL_CLIB == XTHAL_CLIB_NEWLIB
+
+        if (pxCurrentTCB)
+        {
+            if ((uint32_t)_impure_ptr != (uint32_t)(&pxCurrentTCB->xNewLib_reent)) {
+                printf("Task %d, Bad reent ptr\n", val);
+                exit(1);
+            }
+        }
+        else {
+            printf("Task %d, Bad reent ptr in TCB!\n", val);
+            exit(2);
+        }
+#else
+  #error Unsupported C library
+#endif
+
+        test_p = malloc(rand() % 500);
+        if (!test_p) {
+            printf("Task %d, malloc() failed\n", val);
+            exit(3);
+        }
+
+        if (val == 0 && cnt % 100 == 99) {
+        	printf("100...\n");
+        }
+
+        vTaskDelay(1);
+        free(test_p);
+        cnt++;
+    }
+
+#else
+
+    // No thread-safe library, nothing to do
+    test_p = 0;
+    cnt    = 0;
+    vTaskDelay(1);
+
+#endif
+
+    result[val] = 1;
+    vTaskDelete(NULL);
+}
+
+
+/*
+*********************************************************************************************************
+*                                          APP INITIALZATION TASK
+*
+* It initializes the RTOS, creates the other tasks, controls the test and reports the results.
+*
+*********************************************************************************************************
+*/
+
+#define     INIT_TASK_STK_SIZE      (TASK_STK_SIZE_MIN)
+
+static void Init_Task(void *pdata)
+{
+    int t0, t1;
+    int i, busy;
+    int err = 0;
+
+    for (i = 0; i < NTASKS; ++i) {
+        /* Create the application tasks (all are lower priority so wait for us). */
+    	err = xTaskCreate(Task_Func, "Task", TASK_STK_SIZE_STD, (void*)i, 18, &Task_TCB[i]);
+
+        if (err != pdPASS)
+        {
+            printf(TEST_PFX " FAILED to create Task\n");
+            goto done;
+        }
+    }
+
+    /* The test begins here. */
+    t0 = xTaskGetTickCount();
+
+    /* Simulate round-robin of the application tasks every tick. Stop when all results are in. */
+    do {
+        busy = 0;
+        for (i = 0; i < NTASKS; ++i) {
+			//vTaskPrioritySet(Task_TCB[i], 21);
+            vTaskDelay(NTASKS);
+			//vTaskPrioritySet(Task_TCB[i], 22);
+            busy |= result[i] == 0.0;
+        }
+    } while (busy);
+
+    /* The test ends here. Pause to let application tasks shut down cleanly. */
+    t1 = xTaskGetTickCount();
+    //vTaskDelay(NTASKS);
+
+    printf(TEST_PFX " PASSED!\n");
+
+done:
+    #ifdef XT_SIMULATOR
+    /* Shut down simulator and report error code as exit code to host (0 = OK). */
+    exit(0);
+    #endif
+
+    /* Terminate this task. RTOS will continue to run timer, stats and idle tasks. */
+    vTaskDelete(NULL);
+}
+
+/*
+*********************************************************************************************************
+*                                             C ENTRY POINT
+*
+* Initializes FreeRTOS after the platorm's run-time system has initialized the basic platform.
+* Creates at least the first task, which can then create other tasks.
+* Starts multitasking.
+*
+*********************************************************************************************************
+*/
+
+/* Hook functions for standalone tests */
+#ifdef STANDALONE
+
+#if configUSE_TICK_HOOK
+void vApplicationTickHook( void )
+{
+}
+#endif
+
+void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName )
+{
+    /* For some reason printing pcTaskName is not working */
+    puts("\nStack overflow, stopping.");
+    exit(0);
+}
+
+int main(void)
+#else
+int main_xt_clib(int argc, char *argv[])
+#endif
+{
+    int err = 0;
+    int exit_code = 0;
+
+    printf(TEST_PFX " running...\n");
+
+    /* Create the control task initially with the high priority. */
+	err = xTaskCreate(Init_Task, "Init_Task", INIT_TASK_STK_SIZE, NULL, 20, NULL);
+
+
+    if (err != pdPASS)
+    {
+        printf(TEST_PFX " FAILED to create Init_Task\n");
+        goto done;
+    }
+
+    /* Start task scheduler */
+	vTaskStartScheduler();
+
+done:
+    exit_code = err;
+
+#ifdef XT_SIMULATOR
+    /* Shut down simulator and report error code as exit code to host (0 = OK). */
+    exit(exit_code);
+#endif
+
+    /* Does not reach here ('return' statement keeps compiler happy). */
+    return 0;
+}
+
